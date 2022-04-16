c5171782c664100e28dcb66874dca78bab2d06a8
Ítalo <italoherbert@outlook.com>
2022-04-16 13:25:24 -0300

    Após criado programa principal para ler os arquivos de instãncias do diretório e executar os resultados, instância por instância

--- src/caixeiroviajante/metaheuristica/Construcao.cpp
+++ src/caixeiroviajante/metaheuristica/Construcao.cpp
@@ -29,19 +29,15 @@ Solucao Construcao::construcao( double** matrizAdj, int dim ) {
 	while( !nos.empty() ) {
 		int r = rand() % nos.size();
 		int k = nos[ r ];
-	
-		InsercaoInfo melhor;
-		melhor.custo = DBL_MAX;
-		
-		vector<InsersaoInfo> insercaoInfos = this->calculaInsercaoInfos( matrizAdj, dim, sequencia, k );				
-		
+			
+		vector<InsersaoInfo> insercaoInfos = this->calculaInsercaoInfos( matrizAdj, dim, sequencia, k );						
 		sort(insercaoInfos.begin(), insercaoInfos.end(), [](InsercaoInfo a, InsercaoInfo b){
 			return a.custo > b.custo;	
 		});		
 						
 		double alpha = ((double)rand()+1) / (RAND_MAX+1);
 		
-		int r2 = rand() % ( (int)ceil( alpha * insercaoInfos.size() ) );		
+		int r2 = rand() % ( (int)ceil( alpha * insercaoInfos.size() ) );						
 		int i = insercaoInfos[ r2 ].noI;
 		
 		nos.erase( nos.begin() + r );
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,10 @@
 #include <vector>
 #include <ctime>
 #include <cfloat>
+#include <string>
+
+#include <sys/types.h>
+#include <dirent.h>
 
 #include "caixeiroviajante/minhasolucao/CaixeiroViajanteMinhaSolucao.h"
 #include "caixeiroviajante/metaheuristica/CaixeiroViajanteMetaHeuristica.h"
@@ -14,6 +18,75 @@
 #include "util/util.h"
 
 using namespace std;
+
+int main( int argc, char** argv ) {
+	CaixeiroViajanteMetaHeuristica cv;
+	
+	DIR *dir = opendir( "instances/" );
+	struct dirent* in;
+	int cc = 0;
+	while ( ( in = readdir( dir ) ) != NULL ) {		
+		char* inst = (char*) malloc( 100 * sizeof( char ) );
+		strcpy( inst, "instances/" );
+		strcat( inst, in->d_name );
+
+		if ( cc++ < 2 )
+			continue;		
+			
+		argv = (char**) malloc( 2 * sizeof( char* ) );
+		argv[1] = inst;
+		
+		argc = 2;
+	
+					
+	    cv.readTSPData( argc, argv );
+	    		
+		cout << "Buscando solucao..." << endl;
+		
+		int n = 10;
+				
+		double tempoMedio = 0;	
+		double custoMedio = 0;		
+		
+		Solucao melhor;
+		melhor.custo = DBL_MAX;
+		
+		for( int i = 0; i < n; i++ ) {		
+			long long ms1 = get_ms();	
+			Solucao s = cv.calculaCaminho();		
+			long long ms2 = get_ms();
+			
+			double tempo =  ( ms2-ms1 ) / 1000.0;
+			//cout << "Tempo: " << tempo << " seg   Custo: " << s.custo << endl;
+			
+			tempoMedio += tempo;
+			custoMedio += s.custo;
+			
+			if ( s.custo < melhor.custo )
+				melhor = s;
+		}
+		
+		tempoMedio /= n;
+		custoMedio /= n;
+			
+	    cout << argv[1] << " : "<< "T(md)= " << tempoMedio << ";  C(md)= " << custoMedio << endl;
+	    cout << endl;
+	    cout << "T(md)=Tempo médio; C(md)=Custo médio" << endl;
+	}
+	
+	closedir( dir );
+	
+	return 0;
+}
+
+int main22222() {
+	DIR *dir = opendir( "instances/" );
+	struct dirent* in;
+	while ( ( in = readdir( dir ) ) != NULL )
+		cout << in->d_name << endl;
+		
+	return 0;
+}
 
 int main231(int argc, char** argv) {
 	CaixeiroViajanteMetaHeuristica cv;
@@ -36,59 +109,6 @@ int main231(int argc, char** argv) {
 	cv.imprimeSolucao( s );    
     
     return 0;
-}
-
-int main( int argc, char** argv ) {
-	CaixeiroViajanteMetaHeuristica cv;
-	
-	if ( argc < 2 ) {		
-		char* inst = (char*) malloc( 100 * sizeof( char ) );
-		strcpy( inst, "instances/brazil58.tsp" );
-		
-		argv = (char**) malloc( 2 * sizeof( char* ) );
-		argv[1] = inst;
-		
-		argc = 2;
-	}
-					
-    cv.readTSPData( argc, argv );
-    		
-	cout << "Buscando solucao..." << endl;
-	
-	int n = 10;
-			
-	double tempoMedio = 0;	
-	double custoMedio = 0;		
-	
-	Solucao melhor;
-	melhor.custo = DBL_MAX;
-	
-	for( int i = 0; i < n; i++ ) {		
-		long long ms1 = get_ms();	
-		Solucao s = cv.calculaCaminho();		
-		long long ms2 = get_ms();
-		
-		double tempo =  ( ms2-ms1 ) / 1000.0;
-		cout << "Tempo: " << tempo << " seg   Custo: " << s.custo << endl;
-		
-		tempoMedio += tempo;
-		custoMedio += s.custo;
-		
-		if ( s.custo < melhor.custo )
-			melhor = s;
-	}
-	
-	tempoMedio /= n;
-	custoMedio /= n;
-	
-	cout << endl;
-    cout << "Arquivo: " << argv[1] << endl;
-	cout << "Tempo medio: " << tempoMedio << endl;
-	cout << "Custo medio: " << custoMedio << endl;
-	
-	cv.imprimeSolucao( melhor );
-	
-	return 0;
 }
 
 int main2() {
